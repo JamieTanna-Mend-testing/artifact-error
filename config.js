8712ca5f0b57df12868d0608b14e8f208273ccc8
Jamie Tanna <jamie.tanna@mend.io>
2025-12-31 11:51:43 +0000

    qs

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
 module.exports = {
+	onboarding: false,
+	requireConfig: 'ignored',
+
+	branchConcurrentLimit: 100,
+	prConcurrentLimit: 100,
+	// make sure that **??**
+	prHourlyLimit: 1000,
+
 	allowedCommands: [
 		'*',
 	],
